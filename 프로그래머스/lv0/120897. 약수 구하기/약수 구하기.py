ee621c693edf3306c671f305bd3076fffbec08bf
sbtiffanykim <sbtiffanykim@gmail.com>
2023-03-09 10:23:42 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,11 @@
+import math
+
+def solution(n):
+    answer = []
+    for i in range(1, int(math.sqrt(n))+1):
+        if n % i == 0: 
+            answer.append(i)
+            if int(math.sqrt(n)) != n // i:
+                answer.append(n // i)
+    answer.sort()
+    return answer
