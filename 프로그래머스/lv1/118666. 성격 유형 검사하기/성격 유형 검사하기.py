edf8dc8cf519e5f6aef55867d3dc41e6f90f25de
sbtiffanykim <sbtiffanykim@gmail.com>
2023-07-12 01:31:45 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.23 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,7 +1,5 @@
 from collections import defaultdict
 
-
-
 def solution(survey, choices):
     answer = ''    
     scores = {1: (3, 0), 2: (2, 0), 3: (1, 0), 4: (0, 0), 5: (0, 1), 6: (0, 2), 7: (0, 3)}
