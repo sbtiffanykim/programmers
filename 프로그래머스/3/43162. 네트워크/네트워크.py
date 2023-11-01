d289c3b92cc931f09cfa6d032af90aa22befd075
sbtiffanykim <sbtiffanykim@gmail.com>
2023-11-01 10:01:03 +0900

    [level 3] Title: 네트워크, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,23 @@
+from collections import deque
+
 def solution(n, computers):
     answer = 0
-    
     visited = [False] * n
     
     for com in range(n):
         if not visited[com]:
-            dfs(n, com, computers, visited)
+            bfs(n, com, computers, visited)
             answer += 1
-    
     return answer
 
-def dfs(n, com, computers, visited):
+def bfs(n, com, computers, visited):
+    queue = deque()
+    queue.append(com)
     visited[com] = True
-    for i in range(n):
-        if computers[com][i] == 1 and not visited[i]:
-            dfs(n, i, computers, visited)
     
+    while queue:
+        com = queue.popleft()
+        for i in range(n):
+            if not visited[i] and computers[com][i] == 1:
+                queue.append(i)
+                visited[i] = True
