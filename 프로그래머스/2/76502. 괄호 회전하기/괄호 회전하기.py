082a9172c72dba0fc1c83a855cb99dd7bf48654d
sbtiffanykim <sbtiffanykim@gmail.com>
2023-11-03 13:19:56 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,32 @@
+parenthesis = {'}': '{', ')': '(', ']': '['}
+
+def is_valid(s):
+    stack = list()
+
+    flag = True
+    for c in s:
+        if c not in parenthesis:
+            stack.append(c)
+        else:
+            if stack and parenthesis[c] == stack[-1]:
+                stack.pop()
+            else:
+                flag = False
+
+    if stack or not flag:
+        return False
+    return True
+
+
+def solution(s):
+    n = len(s)
+    answer = 0
+    
+    for i in range(n):
+        rotated_str = s
+        if i > 0:
+            rotated_str = s[i:] + s[: (i + n) % n]  # 왼쪽으로 회전
+        if is_valid(rotated_str):  # 올바른 괄호 문자열이면
+            answer += 1
+            
+    return answer
